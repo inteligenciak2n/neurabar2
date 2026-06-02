1ff6c3e84a09f69302876539198029da2aa266e2
Rodrigo Serafim <rdgo.serafim@gmail.com>
2026-06-02 16:56:33 -0300

    remove pasta build controlada por engano

--- public/build/assets/InputLabel-DdGvoNqv.js
+++
@@ -1,1 +0,0 @@
-import{g as e,O as o,I as n,C as t}from"./app-frrp3da3.js";const l={class:"block font-medium text-sm text-gray-700 dark:text-gray-300"},r={key:0},c={key:1},u={__name:"InputLabel",props:{value:String},setup(a){return(s,_)=>(t(),e("label",l,[a.value?(t(),e("span",r,o(a.value),1)):(t(),e("span",c,[n(s.$slots,"default")]))]))}};export{u as _};
